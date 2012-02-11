fe46db325032984a14366d788fe47e3e63595798
David Zarlengo <david.zarlengo@gmail.com>
2012-02-11 14:29:45 -0600

    added a test app

---
+++ spec/support/app.js
@@ -0,0 +1,20 @@
+AModel = Backbone.Model.extend({});
+
+TypicalView = Backbone.View.extend({
+	render: function() {
+		var html = $("<div id='anchor'><p>Hello, World</p></div>");
+		this.$el.append(html);
+		Backbone.outback.bind(this);
+	},
+	remove: function() {
+		Backbone.outback.unbind(this);
+	}
+});
+
+UnobtrusiveView = TypicalView.extend({
+	dataBindings: {
+		'#anchor': { 
+			visible: Backbone.outback.modelRef('isVisible') 
+		}
+	}
+});
