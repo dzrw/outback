9794fee0365a11c3facd875a26246460458cfe56
David Zarlengo <david.zarlengo@gmail.com>
2012-02-11 01:55:27 -0600

    created an unobtrusive test for the visible binding

--- spec/outback.spec.js
+++ spec/outback.spec.js
@@ -1,7 +1,49 @@
 describe('outback.js declarative bindings for backbone', function() {
-	
-	it('should be great', function() {
-		expect(false).toBe(true);
+
+	AModel = Backbone.Model.extend({});
+
+	beforeEach(function(){
+		this.model = new AModel({
+			villain: "mrMonster",
+			doctor: "Seuss",
+			pet: "cat",
+			isValid: false
+		});
+	});
+
+	describe("can be configured in the view rather than in the template", function() {
+
+		UnobtrusiveView = Backbone.View.extend({
+			modelBindings: {
+				visible: Backbone.outback.modelRef('isValid')
+			}
+			render: function() {
+				var html = $("<div id='anchor'><p>Hello, World</p></div>");
+				this.$el.append(html);
+				Backbone.outback.bind(this);
+			},
+			remove: function() {
+				Backbone.outback.unbind(this);
+			}
+		});
+
+		beforeEach(function(){
+			this.view = new UnobtrusiveView({model: this.model});
+			this.view.render();
+			this.el = this.view.$("#anchor");
+		});
+
+		afterEach(function() {
+			this.view.remove();
+		});
+
+		it("should hide the element when the associated model attribute is false", function() {
+			var el;
+			expect(el.is(":visible")).toBeFalsy();
+			this.model.set({isValid: true});
+			expect(el.is(":visible")).toBeTruthy();
+		});
+
 	});
 
 });
