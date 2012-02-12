cb6d3ad253c8d81b6232d571ac21af15983df286
David Zarlengo <david.zarlengo@gmail.com>
2012-02-12 01:13:59 -0600

    added css tests

--- spec/outback.spec.js
+++ spec/outback.spec.js
@@ -366,6 +366,33 @@ describe('outback.js declarative bindings for backbone.js', function() {
 			});
 
 		});
+		
+		describe('the css binding', function() {
+
+			beforeEach(function() {
+				this.model = new AModel({isActive: false});
+				this.view = new FixtureView({model: this.model});
+				_.extend(this.view, {
+					innerHtml: "<span data-bind='css: { active: @isActive }'></span>"
+				})
+
+				this.view.render();
+				this.el = this.view.$('#anchor span');				
+			});
+
+			afterEach(function() {
+				this.view.remove();
+			})
+
+			it('should update the value of the DOM element when the model changes', function() {
+				expect(this.el.hasClass('active')).toBeFalsy();
+
+				this.model.set({isActive: true});
+
+				expect(this.el.hasClass('active')).toBeTruthy();
+			});
+
+		});
 
 		describe('the value binding', function() {
 
@@ -490,3 +517,6 @@ describe('outback.js declarative bindings for backbone.js', function() {
 
 	});
 });
+
+
+
