1711afd1f72652c7fff54677c403ea4a5f61fe96
David Zarlengo <david.zarlengo@gmail.com>
2012-02-12 01:19:21 -0600

    added another class to the css binding test for good measure

--- spec/outback.spec.js
+++ spec/outback.spec.js
@@ -370,10 +370,10 @@ describe('outback.js declarative bindings for backbone.js', function() {
 		describe('the css binding', function() {
 
 			beforeEach(function() {
-				this.model = new AModel({isActive: false});
+				this.model = new AModel({isActive: false, isCool: true});
 				this.view = new FixtureView({model: this.model});
 				_.extend(this.view, {
-					innerHtml: "<span data-bind='css: { active: @isActive }'></span>"
+					innerHtml: "<span data-bind='css: { active: @isActive, cool: @isCool }'></span>"
 				})
 
 				this.view.render();
@@ -386,10 +386,12 @@ describe('outback.js declarative bindings for backbone.js', function() {
 
 			it('should update the value of the DOM element when the model changes', function() {
 				expect(this.el.hasClass('active')).toBeFalsy();
+				expect(this.el.hasClass('cool')).toBeTruthy();
 
-				this.model.set({isActive: true});
+				this.model.set({isActive: true, isCool: false});
 
 				expect(this.el.hasClass('active')).toBeTruthy();
+				expect(this.el.hasClass('cool')).toBeFalsy();
 			});
 
 		});
